0740338d271a2a1bb9de84df6ecb09e17f5f2265
ezgiycel28 <ezgycl28@gmail.com>
2024-05-21 13:05:39 +0300

    Proje dosyası ekle.

---
+++ Asal_Sayılar/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("1.sayiyi giriniz : ");
+        int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("2.sayiyi giriniz : ");
+        int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+        List<int> asalSayilar = AsalSayilariBul(sayi1, sayi2);
+
+        Console.WriteLine($"{sayi1} ile {sayi2} arasındaki asal sayılar:");
+        foreach (int asal in asalSayilar)
+        {
+            Console.WriteLine(asal);
+        }
+        Console.ReadLine();
+    }
+
+    static List<int> AsalSayilariBul(int baslangic, int bitis)
+    {
+        bool[] asalMi = new bool[bitis + 1];
+        for (int i = 2; i <= bitis; i++)
+        {
+            asalMi[i] = true;
+        }
+
+        for (int p = 2; p * p <= bitis; p++)
+        {
+            if (asalMi[p])
+            {
+                for (int i = p * p; i <= bitis; i += p)
+                {
+                    asalMi[i] = false;
+                }
+            }
+        }
+
+        List<int> asalSayilar = new List<int>();
+        for (int i = baslangic; i <= bitis; i++)
+        {
+            if (asalMi[i])
+            {
+                asalSayilar.Add(i);
+            }
+        }
+
+        return asalSayilar;
+
+        Console.ReadLine();
+    }
+}
